db7b61097e9291a7d97da98b51b32a592a793584
Daniel Andres Tobon Comba <106559688+DanielAndresTobonComba@users.noreply.github.com>
2024-01-08 00:39:32 +0000

    actualizado pagina principal

--- nuevo-proyecto/1-Principal/paginaPrincipal.js
+++ nuevo-proyecto/1-Principal/paginaPrincipal.js
@@ -1,20 +1,77 @@
+let botonIngresar = document.getElementById("botonIngresar")
+botonIngresar.addEventListener("click" , agregar)
+ 
+function mostrarDatos (){
 
-function iniciarSesion() {
+    let usuario = document.getElementById("usuario").value
+    let contraseña = document.getElementById("contraseña").value
 
-    let elemento = document.getElementById("tarjetaIniciarSesion")
+    let nombre = document.getElementById("nombre")
+    let acceso = document.getElementById("acceso")
+
+    
+
+    nombre.innerHTML = usuario 
+    acceso.innerHTML = contraseña
+
+    let elemento = document.getElementById("seccionMostrarDatos")
 
 
     if (elemento.style.visibility == "visible") {
         elemento.style.visibility = "hidden"
-        elemento.style.height = "0px"
-        elemento.style.width = "0px"
     }else{
         elemento.style.visibility = "visible"
-        elemento.style.height = "200px"
-        elemento.style.width = "300px"
-        elemento.style.position = "fixed"
-    } 
+    }
+
+
+
+} 
+
+function informacionUsuario(){
+
+
+    let usuario = document.getElementById("usuario").value
+    let contraseña = document.getElementById("contraseña").value
+
+    if(usuario == "" || contraseña == ""){
+        iniciarSesion()
+    }else{
+        mostrarDatos(usuario , contraseña)
+    }
+} 
+
+function iniciarSesion() {
+
+    let elemento = document.getElementById("tarjetaIniciarSesion")
+    let usuario = document.getElementById("usuario").value
+    let contraseña = document.getElementById("contraseña").value
+
+    if(usuario != "" && contraseña !="" || elemento.style.visibility == "visible" ){
+        mostrarDatos()
+        elemento.style.visibility = "hidden"
+
+    } else if (elemento.style.visibility == "visible"){
+        elemento.style.visibility = "hidden"
+    }else{
+        elemento.style.visibility = "visible"
+    }
+    
+    
+
+    
 }
+
+
+
+
+/* function agregar (){
+    let elemento = document.getElementById("tarjetaIniciarSesion")
+    let usuario = document.getElementById("usuario").value
+    let contraseña = document.getElementById("contraseña").value
+    alert("entre")
+
+    mostrarDatos(usuario , contraseña)
+} */
 
 
 
