aca53ccf7f400ea7c7d5f344f4168d499e966eef
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-01-15 12:30:19 -0500

    filtro hecho

--- nuevo-proyecto/4-form-prod/form-prod.js
+++ nuevo-proyecto/4-form-prod/form-prod.js
@@ -1,24 +1,97 @@
 
-/* function mostrar(boton){
-    
-    let elemento = boton.pa
-    if(elemento.style.display == "none"){
-        elemento.style.display = "block"
+function filtrar(contenido){
+    console.log("entre a filtrar con : " + contenido )
+    let contenedor = document.querySelector(".productos")
+    contenedor.innerHTML = `
+            `
+
+    fetch("productos.json")
+        .then(data => data.json())
+        .then(json => {
+            json.productos.forEach(producto =>{
+
+                if(producto.nombre.includes(contenido)){
+                    
+                    let elemento = `
+
+                    <div class="tarjeta" >
+
+                        <div class="textoTarjeta">
+                            
+                            <p>${producto.nombre}</p>
+                            
+                        </div>
+
+                    
+                        <img src="${producto.imagen}" alt="">
+                        
+
+                        
+
+                        <div class="detalleProducto">
+                            
+                            <button class="botonDetalles" onclick="mostrar(this)" >Detalles </button>
+
+                            <section
+                            
+                                class="detalles" >
+                                <p>Tamaño: ${producto.caracteristicas}</p>
+
+                                <p>Precio: $ ${producto.precio}</p>
+
+                                <p></p>
+
+                            </section>
+                        
+                            
+                        </div>
+                        
+
+                        <div class="seccionBotonOrdenar">
+
+                            <button id="botonOrdenar" onclick="identificar(this)" >Ordenar</button>
+
+            
+                        </div>
+
+                    </div>
+                    `
+                    contenedor.innerHTML += elemento
+                    
+                }else if (contenedor.innerHTML.trim() === ""){
+                   contenedor.innerHTML = ` 
+                   <h1>No hay productos con su preferencias de filtro</h1>`
+                }
+            })
+        })
+
+}
+
+function datos(){
+    let entrada = document.getElementById("busqueda")
+    let datos = entrada.value
+    if(datos != ""){
+        filtrar(datos)
     }else{
-        elemento.style.display = "none"
+        insertarProductos()
     }
-} */
+
+
+    
+}
+
+
 
 function insertarProductos() {
 
     let contenedor = document.querySelector(".productos")
-
+    contenedor.innerHTML = `` 
     fetch("productos.json")
         .then(data => data.json())
         .then(json => {
 
             json.productos.forEach(producto => {
-                console.log(producto)
+                //console.log(producto)
 
                 let elemento = `
 
@@ -57,7 +130,7 @@ function insertarProductos() {
 
                     <div class="seccionBotonOrdenar">
 
-                        <button onclick="identificar(this)" >Ordenar</button>
+                        <button id="botonOrdenar" onclick="identificar(this)" >Ordenar</button>
 
         
                     </div>
@@ -75,14 +148,15 @@ function insertarProductos() {
 
 }
 
-insertarProductos()
+
 
 function restar(boton) {
-    
-    // console.log(boton);
+   
+   
     let padre = boton.parentElement;
-    //console.log(padre);
-
+    let seccionBotonOrdenar = boton.parentElement.parentElement;
+    let hijosSeccionBotonOrdenar = seccionBotonOrdenar.children
+    let botonOrdenar = hijosSeccionBotonOrdenar[0]
     let hijos = padre.children;
     let parrafo = hijos[1];
 
@@ -91,6 +165,7 @@ function restar(boton) {
     if (numero <= 0) {
         padre.remove(); // Utiliza el método remove para eliminar el elemento padre
         numero = 0
+        botonOrdenar.style.display = "block"
 
     } else {
         parrafo.textContent = --numero;
@@ -149,5 +224,5 @@ function identificar(boton) {
 
 }
 
-
+insertarProductos()
 
