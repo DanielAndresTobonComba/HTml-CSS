5b92572669108566764b847507212142c62bcdc7
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-01-15 21:38:56 -0500

    facturacion

--- nuevo-proyecto/4-form-prod/form-prod.js
+++ nuevo-proyecto/4-form-prod/form-prod.js
@@ -1,30 +1,111 @@
-
-function crearFactura (tarjeta , cantidad){
-    let contenedor = document.querySelector(".facturaActual")
-    //console.log(tarjeta)
-    console.log(cantidad)
+let ids = []
 
 
-    
-    let hijos = tarjeta.children
-    let padre = hijos[0]
-    let parrafo = padre.children
-    let nombreProducto = parrafo[0].textContent
+function crearFactura(tarjeta, cantidad , accion) {
+    console.log(accion)
+    let contenedor = document.querySelector(".facturaActual");
+    let tabla = document.querySelector("table");
 
-    console.log(nombreProducto)
+    let hijos = tarjeta.children;
+    let padre = hijos[0];
+    let parrafo = padre.children;
+    let nombreProducto = parrafo[0].textContent;
+    let tbody = document.querySelector("tbody")
+
     fetch("productos.json")
-    .then(data => data.json())
-    .then(json => {
-        
-        json.productos.forEach(producto=>{
-            if(producto.nombre == nombreProducto){
-                console.log(producto)
-            }
-        })
+        .then(data => data.json())
+        .then(json => {
 
+            if(accion === "sumar"){
+                tbody.style.visibility = "visible"
+                json.productos.forEach(producto => {
+
+                if(producto.nombre == nombreProducto){
+
+                    let id = producto.id 
+
+                    if(ids.includes(id)==false){
+
+                        let codigo = producto.id;
+                        let nombre = producto.nombre;
+                        let precio = producto.precio;
+                        
+
+                        let fila = tabla.insertRow();
+                        let celdaCodigo = fila.insertCell(0);
+                        let celdaNombre = fila.insertCell(1);
+                        let celdaCantidad = fila.insertCell(2);
+                        let celdaPrecio = fila.insertCell(3);
+    
+                        celdaCodigo.textContent = codigo;
+                        celdaNombre.textContent = nombre;
+                        celdaPrecio.textContent = precio * cantidad;
+                        celdaCantidad.textContent = cantidad;
+                        
+                        
+                        fila.setAttribute("id" , codigo)
+                        contenedor.appendChild(tabla);
+                        console.log("No esta en el arreglo")
+                        ids.push(id)
+                    }else{
+                        let precio = producto.precio;
+                        let fila = document.getElementById(id)
+                        let casillaPrecio = fila.children[3]
+                        let cant = fila.children[2]
+                        
+                        let nuevoPrecio = parseFloat(precio) * cantidad;
+                        
+                        casillaPrecio.textContent = nuevoPrecio;
+
+                        cant.textContent = cantidad
+                        
+                        
+                    }
+
+                }
+            
+                
+            })
+
+        }else if (accion=="restar"){
+            json.productos.forEach(producto => {
+                let tbody = document.querySelector("tbody")
+                if(producto.nombre == nombreProducto){
+
+                    let id = producto.id 
+
+                    
+                    let precio = producto.precio;
+                    let fila = document.getElementById(id)
+                    let casillaPrecio = fila.children[3]
+                    let cant = fila.children[2]
+                    
+                    let nuevoPrecio = parseFloat(precio) * (cantidad);
+                    
+                    casillaPrecio.textContent = nuevoPrecio;
+
+                    cant.textContent = cantidad
+
+                    if(cant.textContent == "0"){
+                        fila.remove()
+                        ids = ids.filter(item => item !== id);
+                    }
+                    
+                        
+                    
+
+                }
+            
+                
+            })
+            
+
+        }
+            
     })
 
 }
+
 
 
 function filtrar(contenido){
@@ -180,7 +261,7 @@ function insertarProductos() {
 
 function restar(boton) {
    
-
+    let tarjeta = boton.parentElement.parentElement.parentElement;
     
     let seccionBotonOrdenar = boton.parentElement.parentElement;
     let hijosSeccionBotonOrdenar = seccionBotonOrdenar.children
@@ -191,14 +272,16 @@ function restar(boton) {
     let parrafo = hijos[1];
 
     let numero = parseInt(parrafo.textContent, 10); // Asegúrase de convertir el texto a un número
+    let accion = "restar"
 
-    if (numero <= 0) {
+    if (numero == 0) {
         padre.remove(); 
         numero = 0
         botonOrdenar.style.display = "block"
 
     } else {
         parrafo.textContent = --numero;
+        crearFactura(tarjeta , numero , accion)
     }
 }
 
@@ -213,8 +296,9 @@ function sumar(boton) {
     let hijos = padre.children;
     let parrafo = hijos[1]
     let cantidad = parrafo.textContent = ++numero
+    let accion = "sumar"
 
-    crearFactura(tarjeta , cantidad )
+    crearFactura(tarjeta , cantidad , accion)
 }
 
 
